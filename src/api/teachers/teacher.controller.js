9fef152d1ecfb565c69c436ef0db921b8570f6b4
Alejandro <frozmiz@live.com>
2023-01-18 23:40:34 +0100

    feat: getById and delete functions

--- src/api/teachers/teacher.controller.js
+++ src/api/teachers/teacher.controller.js
@@ -8,6 +8,16 @@ const indexGet = async(req, res, next) => {
     return res.status(200).json(teachers);
 };
 
+const getById = async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const found = await Teacher.findById(id);
+        return res.status(200).json(found);
+    } catch (error) {
+        return res.status(error.status || 500).json(error.message);
+    }
+};
+
 
 const createPost = async (req, res, next) => {
     try {
@@ -39,8 +49,26 @@ const editPut = async(req, res, next) => {
     }
 }
 
+
+const deleTeacher = async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const deleted = await Teacher.deleteOne({ _id: id});
+        if (deleted.deletedCount) {
+            return res.status(200).json("Elemento eliminado con éxito");   
+        } else {
+            return res.status(200).json("No se encuentra el elemento a eliminar");   
+        }          
+    } catch (error) {
+        res.status(error.status || 500).json(error.message);
+    }
+};
+
+
 module.exports = {
     indexGet,
+    getById,
     createPost,
     editPut,
+    deleTeacher,
 }
--- src/api/teachers/teacher.routes.js
+++ src/api/teachers/teacher.routes.js
@@ -7,10 +7,17 @@ const router = express.Router();
 // ruta: /teachers/
 router.get("/", controller.indexGet);
 
+// ruta: /teachers/xxxxxxxxx
+router.get("/:id", controller.getById);
+
 // Para poder ver un profesor, tenemos que crearlo:
 // ruta: /teachers/create
 router.post("/create", controller.createPost);
 
+// ruta: /teachers/edit/id/xxxxxxxxxx
 router.put("/edit/:id", controller.editPut);
+
+// ruta: /teachers/delete/xxxxxxxxx
+router.delete("/delete/:id", controller.deleTeacher);
 
 module.exports = router;
